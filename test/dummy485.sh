7ace48b5fcccffb63a9a63ed2e6c0bcd5312b830
Andris Pavenis <andris.pavenis@fmi.fi>
2021-10-01 09:54:47 +0300

    Update makefile.inc tests

---
+++ test/dummy485.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+if [ "$1" == "--version" ] ; then echo "gcc (GCC) 4.8.5 20150623 (Red Hat 4.8.5-44)";
+elif  [ "$1" == "-dumpversion" ] ; then echo "4.8.5"; fi
---
+++ test/dummy841.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+if [ "$1" == "--version" ] ; then echo "gcc (GCC) 8.4.1 20200928 (Red Hat 8.4.1-1)";
+elif [ "$1" == "-dumpversion" ] ; then echo "8"; fi
