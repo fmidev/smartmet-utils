ed55acdb3047b64a432fa7b4480e6435614ba2d8
Andris Pavēnis <andris.pavenis@fmi.fi>
2025-02-20 13:13:07 +0200

    [tests] Update test for detection options for proj

--- test/check-proj.mk
+++ test/check-proj.mk
@@ -8,5 +8,8 @@ all:
 	@/bin/echo PROJ_LIBS=$(PROJ_LIBS)
 	@/bin/echo REQUIRED_LIBS=$(REQUIRED_LIBS)
 	@/bin/echo CFLAGS=$(CFLAGS)
+# Verify that pkg-config actually founds proj (could fail also due to missing dependency,
+# makefile.inc redirects pkg-config stderr to /dev/null, so we must check once more here)
+	PKG_CONFIG_PATH=$(PROJ_PKG_SEARCH) pkg-config --cflags --libs $(REQUIRES)
 	( echo "#include <proj.h>"; echo "int main() { return 0; }" ) |\
 		$(CXX) -x c++ $(CFLAGS) $(INCLUDES) - $(REQUIRED_LIBS) -o /dev/null
